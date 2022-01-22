39b2e535f1d08afd6d50219e9cf5687f1182efdc
1nchhh <76808801+1nchhh@users.noreply.github.com>
2022-01-21 20:42:28 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@ const {
 
 async function main() {
     console.log('starting')
-    await gen.generateRepl('1nchhh/reputilv43928748793264982364897623', '')
+    await gen.generateRepl('1nchhh/reputilv43928748793264982364897623', 's%3AemE6pcfvOK6cRy7GaQxs5gB0OFBJ9qY9.wQvEzCgSMb1w8i3peXzMiH2XmA592xOm7GeXOZbK2tU')
     console.log('made repl')
     main()
 }

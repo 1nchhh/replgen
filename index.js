781c89e2930555182369b40f330636813e1567e6
1nchhh <76808801+1nchhh@users.noreply.github.com>
2022-01-21 20:21:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 const {
     Worker
 } = require('worker_threads');
-const input = require('input');
 
-input.text('threads').then(threads => {
-    let t = parseInt(threads)
-    function initWorker(i) {
-        const worker = new Worker('./worker.js', {
-            workerData: {
-                count: process.argv[2],
-                id: i
-            }
-        });
-        worker.on('message', message => {
-            console.log(message);
-        });
-        worker.on('error', error => {
-            console.log(error);
-            initWorker(i)
-        })
-        worker.on('exit', code => {
-            console.log(`Worker stopped with exit code ${code}`);
-            initWorker(i)
-        });
-    }
-    for (let i = 0; i < t; i++) {
+let t = parseInt(parseInt(process.argv[3]))
+function initWorker(i) {
+    const worker = new Worker('./worker.js', {
+        workerData: {
+            count: process.argv[2],
+            id: i
+        }
+    });
+    worker.on('message', message => {
+        console.log(message);
+    });
+    worker.on('error', error => {
+        console.log(error);
         initWorker(i)
-    }
-});
+    })
+    worker.on('exit', code => {
+        console.log(`Worker stopped with exit code ${code}`);
+        initWorker(i)
+    });
+}
+for (let i = 0; i < t; i++) {
+    initWorker(i)
+}

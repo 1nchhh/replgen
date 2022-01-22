07ea629cd7556ef73a4424b00f1746ab45b6efd6
1nchhh <76808801+1nchhh@users.noreply.github.com>
2022-01-21 20:39:13 -0600

    Update puppeteer.js

--- puppeteer.js
+++ puppeteer.js
@@ -19,7 +19,6 @@ async function init(i) {
             userDataDir: './userData' + (i ? `-${i}` : ''),
             args: [
                 '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
-                '--use-gl=desktop',
                 '--no-sandbox'
             ],
         })
